4eba9f93e46bce2118694d76349f0b783bd75a10
lukewnag <102311628+lukewnag@users.noreply.github.com>
2023-03-23 09:47:48 -0400

    add navbar

--- js/index.js
+++ js/index.js
@@ -27,13 +27,24 @@ function switchImage() {
 
 var coll = document.getElementsByClassName("collapsible");
 for (var i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
+
+const mobileBtn = document.getElementById('mobile-cta')
+    nav = document.querySelector('nav')
+    mobileBtnExit = document.getElementById('mobile-exit')
+
+    mobileBtn.addEventListener('click', () => {
+        nav.classList.add('menu-btn')
+    })
+    mobileBtnExit.addEventListener('click', () => {
+        nav.classList.remove('menu-btn')
+    })
